a1adc54a342638c8ce1909dafc5de33d135f92da
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-05-18 15:52:29 -0400

    Add Mapped Data to Components

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 function Entry(props) {
   return (
     <div className="term">
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Emojis from "emoji-book"
+import Entry from "/components/Entry"
 
+
+
+
+function CreateEmojiEntry(emojiTerm){
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    {Emojis.map(CreateEmojiEntry)}
+  </div>,
   document.getElementById('root')
 );
